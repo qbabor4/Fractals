bb3fca5b75d0700e3dd457f2350516f0963f1159
jakub <qbabor4444@gmail.com>
2017-03-01 18:16:36 +0100

    changes in js; clear code

--- js/fractals.js
+++ js/fractals.js
@@ -1,5 +1,7 @@
 
+//Mandelbrot fractal
 
+//  canvas variables
 var canvas = $("#myCanvas")[0] //dostaje obiekt jquery a nie canvas jak jest bez [0]
 var canvasContent = canvas.getContext("2d");
 var canvasWidth = canvas.width;
@@ -12,12 +14,12 @@ var panx = -100;
 var pany = 0;
 var zoom = 150;
 
-// Palette array of 256 colors
+// Palette array of 256 colors in object {r,g,b}
 var palette = [];
     
 // define values od a pixel
 function drawPixel(x, y, r, g, b, a) {
-    var index = ( x + y * canvasWidth) * 4  ; // data w canvasData jest jako lista, a nie macierz
+    var index = ( x + y * canvasWidth) * 4  ; // data in canvasData ia an array, not matrix
     
     canvasData.data[index] = r;
     canvasData.data[index +1] = g;
@@ -35,8 +37,7 @@ function updateCanvas() {
 function iterate(x, y, imageWidth, imageHeight, maxiterations) {
     // Convert the screen coordinate to a fractal coordinate
     var x0 = (x + offsetx + panx) / zoom;
-    var y0 = (y + offsety + pany) / zoom;
-    
+    var y0 = (y + offsety + pany) / zoom;  
        
     // Iteration variables
     var a = 0;
@@ -52,64 +53,60 @@ function iterate(x, y, imageWidth, imageHeight, maxiterations) {
         a = rx;
         b = ry;
         iterations++;
-    }
-        
+    }    
     // Get palette color based on the number of iterations
     var color;
     if (iterations == maxiterations) {
-        color = { r:0, g:0, b:0}; // Black
+        color = { r:0, g:0, b:0 }; // Black
     } else {
         var index = Math.floor((iterations / (maxiterations-1)) * 255);
         color = palette[index];
-    }
-        
+    }    
     return color;
 }
 
+// Calculate a gradient
 function generatePalette() {
-        // Calculate a gradient
-        var roffset = 24;
-        var goffset = 16;
-        var boffset = 0;
-        for (var i=0; i<256; i++) {
-            palette[i] = { r:roffset, g:goffset, b:boffset};
-            
-            if (i < 64) {
-                roffset += 3;
-            } else if (i<128) {
-                goffset += 3;
-            } else if (i<192) {
-                boffset += 3;
-            }
+    var roffset = 24;
+    var goffset = 16;
+    var boffset = 0;
+    
+    for (var i=0; i<256; i++) {
+        palette[i] = { r:roffset, g:goffset, b:boffset};    
+        
+        if (i < 64) {
+            roffset += 3;
+        } else if (i<128) {
+            goffset += 3;
+        } else if (i<192) {
+            boffset += 3;
         }
+    }
 }
 
-generatePalette();
-console.log(palette);
-
-var color = iterate(1,2, canvasWidth, canvasHeight, 250); 
-console.log(color.r, color.g, color.b);
-color = iterate(2,4, canvasWidth, canvasHeight, 250); 
-console.log(color.r, color.g, color.b);
-color = iterate(150,180, canvasWidth, canvasHeight, 250); 
-console.log(color.r, color.g, color.b);
-
-
-// draw pixels
-for (var i = 0; i < canvasHeight + 1; i++) {
-    for (var j = 0; j < canvasWidth + 1; j++){
-        //drawPixel(j, i, 20,200,10,255); 
-        color = iterate(j, i, canvasWidth, canvasHeight, 250);
-        //console.log(color.r, color.g, color.b);
-        drawPixel(j, i, color.r, color.g, color.b, 255); 
-    } 
+// change color of all pixels in order to create fractal
+function drawFractal(){
+    // draw pixels
+    for (var i = 0; i < canvasHeight + 1; i++) {
+        for (var j = 0; j < canvasWidth + 1; j++){
+            //drawPixel(j, i, 20,200,10,255); 
+            color = iterate(j, i, canvasWidth, canvasHeight, 250);
+            //console.log(color.r, color.g, color.b);
+            drawPixel(j, i, color.r, color.g, color.b, 255); 
+        } 
+    }
+    updateCanvas();
 }
 
-updateCanvas();
-/*
-canvasContent.moveTo(0,0);
-canvasContent.lineTo(200,100);
-canvasContent.moveTo(200,100);
-canvasContent.lineTo(300,100);
-canvasContent.stroke();
-*/
+
+
+function init(){
+    generatePalette();
+}
+
+function main(){
+    init();
+    drawFractal();
+}
+
+main();
